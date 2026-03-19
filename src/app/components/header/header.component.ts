5a56af0a8c717ae60c648599676d16896c5f88f7
AmberLilith <amber.lilith.ordone@gmail.com>
2026-03-19 18:55:14 -0300

    Incluido botao instalar

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, HostListener, inject } from '@angular/core';
 import { ThemeService } from '../../services/theme-service/theme-service.service';
 import { IconComponent } from '../icon/icon.component';
 import { SideMenuComponent } from '../side-menu/side-menu.component';
@@ -13,4 +13,29 @@ import { SideMenuComponent } from '../side-menu/side-menu.component';
 export class HeaderComponent {
   themeService = inject(ThemeService);
   sidebarOpen: boolean = false;
+  deferredPrompt: any;
+  showInstallButton = false;
+
+  @HostListener('window:beforeinstallprompt', ['$event'])
+  onBeforeInstallPrompt(e: Event) {
+    // Impede o Chrome de mostrar o aviso automático
+    e.preventDefault();
+    // Guarda o evento para usar depois
+    this.deferredPrompt = e;
+    // Mostra o seu botão customizado
+    this.showInstallButton = true;
+  }
+
+  installPWA() {
+    this.showInstallButton = false;
+    // Dispara o prompt guardado
+    this.deferredPrompt.prompt();
+    // Verifica a escolha do usuário
+    this.deferredPrompt.userChoice.then((choiceResult: { outcome: string }) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('Usuário aceitou a instalação');
+      }
+      this.deferredPrompt = null;
+    });
+  }
 }
